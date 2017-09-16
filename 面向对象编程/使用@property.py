40d18c23bc27f665147a32196d2d11dc806f328f
skywalker <skywalker_x@163.com>
2017-09-16 18:57:01 +0800

    property案例

--- 面向对象编程/使用@property.py
+++ 面向对象编程/使用@property.py
@@ -16,7 +16,7 @@ class Student(object):
         if not isinstance(value, int):
             raise ValueError('score must be an integar')
         if value < 0 or value > 100:
-            raise ValueError('score must bettween 0 ~ 100')
+            raise ValueError('score must between 0 ~ 100')
         self._score = value
 
 
@@ -24,3 +24,42 @@ stu = Student(99)
 print(stu.score)
 stu.score = 50
 print(stu.score)
+
+
+#  请利用@property给一个Screen对象加上width和height属性，以及一个只读属性resolution：
+class Screen(object):
+    def __init__(self, width=None, height=None):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def resolution(self):
+        return 'width: %s, height: %s' % (self.__width, self.__height)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+
+scr = Screen()
+scr.width = 50
+scr.height = 100
+print(scr.width)
+print(scr.height)
+print(scr.resolution)
+
+scr2 = Screen(30, 100)
+print(scr2.height)
+print(scr2.width)
